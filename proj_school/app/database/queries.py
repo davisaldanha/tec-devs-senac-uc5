f90763d17bb75e1097a4d3cbbc859feae492c59d
Davi Saldanha <davisaldanha.ti@gmail.com>
2025-12-18 10:23:12 -0300

    Add class-related SQL queries for database operations

--- proj_school/app/database/queries.py
+++ proj_school/app/database/queries.py
@@ -15,3 +15,29 @@ DELETE_STUDENT = '''DELETE FROM tbl_aluno WHERE id_aluno = %s'''
 SELECT_ALL_STUDENT = '''SELECT * FROM tbl_aluno'''
 
 SELECT_STUDENT_ID = '''SELECT * FROM tbl_aluno WHERE id_aluno = %s'''
+
+MATRICULATION = '''UPDATE tbl_aluno SET fk_turma = %s WHERE id_aluno = %s'''
+
+
+# -------------------
+# -- QUERIES CLASS
+# -------------------
+
+SELECT_CLASS_ID = '''SELECT * FROM tbl_turma WHERE id_turma = %s'''
+
+SELECT_ALL_CLASS = '''SELECT * FROM tbl_turma '''
+
+SELECT_CLASS_PROF = '''SELECT * FROM tbl_turma WHERE fk_professor = %s'''
+
+SELECT_CLASS_SHIFT = '''SELECT * FROM tbl_turma WHERE turno = %s'''
+
+SELECT_CLASS_BETWEEN_DATE = '''SELECT * FROM tbl_turma WHERE criado_em BETWEEN %s AND %s'''
+
+SELECT_CLASS_COURSE = '''SELECT * FROM tbl_turma WHERE fk_curso = %s'''
+
+INSERT_CLASS = '''INSERT INTO tbl_turma(fk_professor, fk_curso, turno)
+                    VALUES (%s, %s, %s)'''
+
+UPDATE_CLASS = '''UPDATE tbl_turma SET fk_professor = %s WHERE id_turma = %s'''
+
+DELETE_CLASS = '''DELETE FROM tbl_turma WHERE id_turma = %s'''
