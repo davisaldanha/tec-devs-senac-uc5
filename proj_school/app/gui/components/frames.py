ca83c7db5a7c999d7d5f435a7615d14f71bc4b92
Davi Saldanha <davisaldanha.ti@gmail.com>
2026-01-27 11:18:09 -0300

    Refactor CardFrame class to enhance card display functionality

--- proj_school/app/gui/components/frames.py
+++ proj_school/app/gui/components/frames.py
@@ -1,9 +1,29 @@
-import customtkinter as tck
+import customtkinter as ctk
 
-class MyFrame(ctk.CTkFrame):
-    def __init__(self, master, **kwargs):
-        super().__init__(master, **kwargs)
+from app.gui.styles.fonts import *
 
-        # add widgets onto the frame, for example:
-        self.label = ctk.CTkLabel(self)
-        self.label.grid(row=0, column=0, padx=20)
+class CardFrame(ctk.CTkFrame):
+    '''Frame customizado para exibir cards informativos'''
+    def __init__(self, master, title='', **kwargs):
+        super.__init__(
+            master,
+            fg_color='white',
+            corner_radius=10,
+            border_width=1,
+            border_color='#E0E0E0',
+            **kwargs,
+        )
+    
+        # Título do card
+        title_label = ctk.CTkLabel(
+            self,
+            text=title,
+            font=FONT_TITLE,
+            text_color='#333333'
+        )
+        title_label.pack(padx=15, pady=(15, 10), anchor="w")
+
+        # Frame de conteúdo
+        self.content_frame = ctk.CtkFrame(self, fg_color='trasparent')
+        self.content_frame.pack(fill='both', expand=True, padx=15, pady=(0, 15))
+        
